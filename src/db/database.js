68d39ea007dbb13fc82f98cc5b4ae543f776c14c
Shuaib464 <shuaibshekh464@gmail.com>
2024-02-24 15:42:05 +0530

    add database connection file(function)

---
+++ src/db/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URL)
+        console.log(`MONGODB connected !! DB HOST: ${connectionInstance}`);
+    } catch (error) {
+        console.log("MONGODB connection failed");
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
