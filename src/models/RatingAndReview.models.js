1e93b4d3dcf47ec27ea7ee4db8e3a2271dd2c070
Shuaib464 <shuaibshekh464@gmail.com>
2024-02-29 15:31:58 +0530

    course property added in RatingAndReview model

--- src/models/RatingAndReview.models.js
+++ src/models/RatingAndReview.models.js
@@ -15,6 +15,12 @@ const ratingAndReviewSchema = new mongoose.Schema(
             type: String,
             required: true,
             trim: true,
+        },
+        course: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Course",
+            required: true,
+            index: true,
         }
     }
 );
